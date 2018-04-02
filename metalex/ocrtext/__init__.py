73d082dad4df1bc3e8ff1ac5b66a686fda497065
Elvis MBONING <levismboning@yahoo.fr>
2018-04-03 01:02:52 +0200

    Adding ocr with ocropy

--- metalex/ocrtext/__init__.py
+++ metalex/ocrtext/__init__.py
@@ -6,34 +6,34 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 """
-    MetaLex is general tool for lexicographic and metalexicographic activities
-    Copyright (C) 2017  by Elvis MBONING
+MetaLex is general tool for lexicographic and metalexicographic activities
+Copyright (C) 2017  by Elvis MBONING
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    
-    Contact : levismboning@yahoo.fr
-    
-    ---------------------------------------------------------------------------
-    
-    MetaLex module
-    
-    Ce module traite, c'est à dire améliore la qualité des images soumises en entrée
-    du programme, et les ocrisent par la suite.
-    
-    Functions :
-        * Traitment of image files with PIL.image
-        * Ocrisation of image files with tesseract
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact : levismboning@yahoo.fr
+
+---------------------------------------------------------------------------
+
+MetaLex module
+
+Ce module améliore la qualité des images soumises en entrée
+du programme, et les ocrisent par la suite.
+
+Functions :
+    * Traitment of image files with PIL.image
+    * Ocrisation of image files with tesseract
         
 """
 
@@ -43,9 +43,9 @@ __version__ = "1.8.5"
 # ----Internal Modules------------------------------------------------------
 
 from .normalizeImage  import EnhanceImages, get_images
-from .makeOcr         import BuildOcrImages
+from .makeOcr         import BuildOcrTesserocr, BuildOcrOcropy
 from .normalizeText   import BuildTextWell, FileRule
-from .wordsCorrection import correct_word, caract_replace, word_replace
+from .wordsCorrection import correct_word, caract_replace, word_replace 
 
 # -----Global Variables-----------------------------------------------------
 
